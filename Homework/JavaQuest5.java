e92eebcdf3a82dcea991d10e4ac22b881d709b26
jyc058 <jacky19970508@gmail.com>
2025-04-14 01:20:17 +0800

    update

---
+++ Homework/JavaQuest5.java
@@ -0,0 +1,30 @@
+public class JavaQuest5 {
+  /**
+   * Expected output: The index of the last character of c is 11
+   */
+  public static void main(String[] args) {
+    String str = "coding bootcamp."; // You should not change this line
+    char target = 'c';
+
+    // if found, print "Found."
+    // if not found, print "Not Found."
+    // code here ...
+    if (str.length() -1 == 11) {
+      if (str.charAt(11) == 'c') {
+      }
+        System.out.println("Found");
+       } else {
+        System.out.println("not found");
+      }
+    }
+    
+
+
+      
+    
+  }
+
+
+
+
+
